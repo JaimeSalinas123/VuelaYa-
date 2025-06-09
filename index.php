018adf7d961996713f250308fd496b52ca4d4f34
Jaime Nelson Lopez Salinas <jnlopezjr36@gmail.com>
2025-06-08 21:57:49 -0600

    Se sumo lo que es parte del index
    Se hiceron varios cambios como lo que es el buscador de vuelos, y un par de recomendaciones que se pusieron.

--- index.php
+++ index.php
@@ -45,5 +45,89 @@
             <h2 class="introh3">Ofertas de vuelo barato</h2>
         </div>
     </header>
+
+<div class="vuelobuscador">
+    <div class="buscador">
+        <div class="buscadorcolumna">
+            <label>
+                <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+                </svg>
+                Desde
+            </label>
+            <input type="text" placeholder="Tu origen">
+        </div>
+        <div class="buscadorcolumna">
+            <label>
+                <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M20.25 14.15v4.25c0 1.094-.787 2.036-1.872 2.18-2.087.277-4.216.42-6.378.42s-4.291-.143-6.378-.42c-1.085-.144-1.872-1.086-1.872-2.18v-4.25m16.5 0a2.18 2.18 0 0 0 .75-1.661V8.706c0-1.081-.768-2.015-1.837-2.175a48.114 48.114 0 0 0-3.413-.387m4.5 8.006c-.194.165-.42.295-.673.38A23.978 23.978 0 0 1 12 15.75c-2.648 0-5.195-.429-7.577-1.22a2.016 2.016 0 0 1-.673-.38m0 0A2.18 2.18 0 0 1 3 12.489V8.706c0-1.081.768-2.015 1.837-2.175a48.111 48.111 0 0 1 3.413-.387m7.5 0V5.25A2.25 2.25 0 0 0 13.5 3h-3a2.25 2.25 0 0 0-2.25 2.25v.894m7.5 0a48.667 48.667 0 0 0-7.5 0M12 12.75h.008v.008H12v-.008Z" />
+                </svg>
+                Hacia
+            </label>
+            <input type="text" placeholder="Tu Destino">
+        </div>
+        <div class="buscadorcolumna">
+            <label>
+                <svg class="input-icon" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M2.25 18.75a60.07 60.07 0 0 1 15.797 2.101c.727.198 1.453-.342 1.453-1.096V18.75M3.75 4.5v.75A.75.75 0 0 1 3 6h-.75m0 0v-.375c0-.621.504-1.125 1.125-1.125H20.25M2.25 6v9m18-10.5v.75c0 .414.336.75.75.75h.75m-1.5-1.5h.375c.621 0 1.125.504 1.125 1.125v9.75c0 .621-.504 1.125-1.125 1.125h-.375m1.5-1.5H21a.75.75 0 0 0-.75.75v.75m0 0H3.75m0 0h-.375a1.125 1.125 0 0 1-1.125-1.125V15m1.5 1.5v-.75A.75.75 0 0 0 3 15h-.75M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Zm3 0h.008v.008H18V10.5Zm-12 0h.008v.008H6V10.5Z" />
+                </svg>
+                Presupuesto
+            </label>
+            <input type="text" placeholder="Tu Presupuesto">
+        </div>
+    </div>
+</div>
+<div class="cuadro-simbolico"></div>
+
+<div class="recomendado">
+    <div class="div-recomendado">
+        <h2 class="recomendacion">Recomendaciones para tus próximos viajes</h2>
+        <div class="carta-recomendacion">
+            <div class="carta">
+                <img src="recursos/arg.jpg" alt="Argentina" class="img">
+                <h3>Buenos Aires</h3>
+                <p class="precio">$350</p>
+                <p class="duracion">Vuelo Promedio de 14 horas</p>
+                <p class="ida-vuelta">Ida y vuelta</p>
+                <button class="ver-vuelos">Ver Vuelos</button>
+                <p class="warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                    </svg>
+                    El precio puede cambiar dependiendo de la aerolínea
+                </p>
+            </div>
+            <div class="carta">
+                <img src="recursos/alemania.jpg" alt="Alemania" class="img">
+                <h3>Munich</h3>
+                <p class="precio">$800</p>
+                <p class="duracion">Vuelo Promedio de 23 horas</p>
+                <p class="ida-vuelta">Ida y vuelta</p>
+                <button class="ver-vuelos">Ver Vuelos</button>
+                <p class="warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                    </svg>
+                    El precio puede cambiar dependiendo de la aerolínea
+                </p>
+            </div>
+            <div class="carta">
+                <img src="recursos/york.jpg" alt="New York" class="img">
+                <h3>New York</h3>
+                <p class="precio">$200</p>
+                <p class="duracion">Vuelo Promedio de 4 horas</p>
+                <p class="ida-vuelta">Ida y vuelta</p>
+                <button class="ver-vuelos">Ver Vuelos</button>
+                <p class="warning">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
+                    </svg>
+                    El precio puede cambiar dependiendo de la aerolínea
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
+
 </body>
 </html>
